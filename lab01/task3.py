c31c9b14da64d1ba9337910b418122311a905fcc
anuszek <153279378+anuszek@users.noreply.github.com>
2025-03-12 12:13:14 +0100

    task3

--- lab01/task3.py
+++ lab01/task3.py
@@ -14,28 +14,25 @@ N = 512  # block length
 prefix_starts = np.zeros(K)
 
 for k in range(K):
-    idx_start = k * (M + N)
-    prefix = x[idx_start:idx_start + M]
+    idx_start = (k + 1) * N
+    prefix = x[idx_start-M-1:idx_start-1]
 
     # cross correlation between prefix and signal
     r = np.correlate(x, prefix, mode='full')
-    lags = np.arange(-(len(r)//2), len(r)//2 + 1)
 
     # find the index of the prefix start in the signal
-    idx = np.argmax(np.abs(r))
-    current_prefix_start = lags[idx]
+    idx = np.argmax(r)
+    prefix_starts[k] = idx
 
-    prefix_starts[k] = current_prefix_start 
+print(np.abs(prefix_starts - M + 1))
 
-print(np.abs(prefix_starts))
-
-import matplotlib.pyplot as plt
-plt.figure(figsize=(12, 6))
-plt.plot(x)
-for start in prefix_starts:
-  plt.axvline(x=int(start), color='r', linestyle='--')
-plt.title('Signal with Detected Prefixes')
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
-plt.grid(True)
-plt.show()
+# import matplotlib.pyplot as plt
+# for start in prefix_starts:
+#     plt.figure(figsize=(12, 6))
+#     plt.plot(x)
+#     plt.axvline(x=int(start), color='r', linestyle='--')
+# plt.title('Signal with Detected Prefixes')
+# plt.xlabel('Sample')
+# plt.ylabel('Amplitude')
+# plt.grid(True)
+# plt.show()
